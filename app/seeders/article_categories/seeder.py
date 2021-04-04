27ddc11c96b88f6565f8638858c44425dc2b16db
Collins Liyosi <collinskivale@gmail.com>
2021-04-05 01:21:19 +0300

    Sync article categories

--- app/seeders/article_categories/seeder.py
+++ app/seeders/article_categories/seeder.py
@@ -1,6 +1,9 @@
 import logging
 
+from bs4 import BeautifulSoup
+from django.conf import settings
 from seeders.seeder import DatabaseSeeder
+from articles.models import Category
 
 logger = logging.getLogger(__name__)
 
@@ -9,6 +12,26 @@ class ArticleCategorySeeder(DatabaseSeeder):
     """
     Seed all the article categories from the provided xml file
     """
+    DATASOURCE_FILE = settings.BASE_DIR.joinpath(
+        'seeders/article_categories/categories.xml'
+    )
 
     def seed(self) -> None:
-        logger.info("Seeding Article Categories")
+        logger.info('Seeding Article Categories')
+        # from pdb import set_trace; set_trace()
+
+        with open(self.DATASOURCE_FILE) as file:
+            parsed_file = BeautifulSoup(file, 'xml')
+            category_elements = parsed_file.findAll('set')
+
+            for category_element in category_elements:
+                category_name = category_element.setName.text
+                category_code = category_element.setSpec.text
+
+                try:
+                    Category.objects.get(code=category_code)
+                except Category.DoesNotExist:
+                    Category.objects.create(
+                        name=category_name,
+                        code=category_code
+                    )
