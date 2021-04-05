79bbe4d530ae01636cb6f91ec2331d6ffcf31655
Collins Liyosi <collinskivale@gmail.com>
2021-04-05 19:13:13 +0300

    Implement articles list and detail view

--- app/articles/views.py
+++ app/articles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.db.models import QuerySet
+from django.views.generic import ListView, DetailView
+
+from articles.models import Article
+
+
+class ArticlesView(ListView):
+    template_name = 'articles/list.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self) -> QuerySet:
+        return Article.objects.all().order_by('-created_on')[: 20]
+
+
+class ArticlesDetailView(DetailView):
+    model = Article
+    template_name = 'articles/detail.html'
