225d9d04fdb2b3ea14ec4c1b9611b77275b596f7
Collins Liyosi <collinskivale@gmail.com>
2021-04-06 18:23:14 +0300

    Add listing of article authors

--- app/articles/tests/views/test_articles.py
+++ app/articles/tests/views/test_articles.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable
 
 import pytest
-from articles.models import Article, Category
+from articles.models import Article, ArticleAuthor, Author, Category
 from django.test import Client
 from django.urls import reverse
 
@@ -12,11 +12,46 @@ def test_list_articles(
     category: Category,
     create_article: Callable[[Any, Category], Callable[[Category], Article]]
 ):
-    article1: Article = create_article(category)
-    article2: Article = create_article(category)
+    article1 = create_article(category)
+    article2 = create_article(category)
     url = reverse('articles:index')
     response = client.get(url)
     assert response.status_code == 200
     assert 'articles/list.html' in response.template_name
     assert article1.title in response.rendered_content
     assert article2.title in response.rendered_content
+
+
+@pytest.mark.django_db
+def test_retrive_article(
+    client: Client,
+    category: Category,
+    create_article: Callable[[Any, Category], Callable[[Category], Article]]
+):
+    article = create_article(category)
+    url = reverse('articles:detail', kwargs={'pk': article.id})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'articles/detail.html' in response.template_name
+    assert article.title in response.rendered_content
+
+
+@pytest.mark.django_db
+def test_retrive_article_should_show_authors(
+    client: Client,
+    category: Category,
+    author: Author,
+    create_article: Callable[[Any, Category], Callable[[Category], Article]]
+):
+    article = create_article(category)
+    article_author = ArticleAuthor.objects.create(
+        author=author,
+        article=article
+    )
+    article_author = ArticleAuthor.objects.get(author=author, article=article)
+    url = reverse('articles:detail', kwargs={'pk': article.id})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'articles/detail.html' in response.template_name
+    assert article.title in response.rendered_content
+    assert article_author.author.name in response.rendered_content
